edb557b2a28041debdddf3c45a93d79fada5235a
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-12-07 16:34:13 +0700

    update cardus app project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Cardus App',
+    description: `Cardus is a web app that allows users to create and manage their own warehouse of items. Users can add items to their warehouse, and easily identify what box is storing what item. Users can also share their warehouse or box with other users, allowing them to collaborate together.`,
+    imgSrc: '/static/images/cardus-app.png',
+    href: 'https://cardus.vercel.app/',
+  },
+  {
     title: 'Hujian',
     description: `Superhero for student. Not from MCU, but still SUPER. Its power is submit form on time.`,
     imgSrc: '/static/images/hujian.png',

bbfc11cb310b1107955ab3a57381a85b5dfbec3c
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-12-20 10:37:54 +0700

    fix: remove 2 projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,18 +5,6 @@ const projectsData = [
     imgSrc: '/static/images/cardus-app.png',
     href: 'https://cardus.vercel.app/',
   },
-  {
-    title: 'Hujian',
-    description: `Superhero for student. Not from MCU, but still SUPER. Its power is submit form on time.`,
-    imgSrc: '/static/images/hujian.png',
-    href: 'https://imbios.github.io/Hujian',
-  },
-  {
-    title: 'Gubuk Imam Chat App',
-    description: `Just chat me through this app`,
-    imgSrc: '/static/images/gubuk-imam.png',
-    href: 'http://gubuk-imam.vercel.app/',
-  },
 ]
 
 export default projectsData
